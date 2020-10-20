6a400a5dcf1f97976c8aa97ebb53389cb6fe1ed4
vfrezende <vitorfranco12@gmail.com>
2020-10-20 10:26:47 -0300

    Commit 15

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -2,8 +2,12 @@ public class NewReleasePrice extends Price {
    public int getPriceCode() {
        return Movie.NEW_RELEASE;
    }
-   
+
    public double getCharge(int daysRented){
       return daysRented * 3;
    } 
+   
+   public int getFrequentRenterPoints(int daysRented) {
+       return (daysRented > 1) ? 2: 1;
+   }
 } 
--- Price.java
+++ Price.java
@@ -1,10 +1,7 @@
 public abstract class Price {
    	public abstract int getPriceCode();
 	public abstract double getCharge(int daysRented);
-    public int getFrequentRenterPoints(int daysRented) {
-      if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-        return 2;
-      else
-        return 1;
-    }
+   	public int getFrequentRenterPoints(int daysRented) {
+       return 1;
+   	}
 } 
