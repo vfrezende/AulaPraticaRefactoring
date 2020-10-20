ca48b96934da6286ea0af09e0b38688609d43f47
vfrezende <vitorfranco12@gmail.com>
2020-10-20 10:25:06 -0300

    Commit 14

--- Movie.java
+++ Movie.java
@@ -39,10 +39,7 @@ public class Movie {
         return _price.getCharge(daysRented);
     }
 
-    public int getFrequentRenterPoints(int daysRented){
-      // add bonus for a two day new release rental
-      if ((getPriceCode() == Movie.NEW_RELEASE) &&
-          daysRented > 1) return 2;
-      else return 1;
+    public int getFrequentRenterPoints(int daysRented) {
+      return _price.getFrequentRenterPoints(daysRented);
     }
 } 
