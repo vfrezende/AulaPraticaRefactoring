442a2057e6838be802dc5a26be047a77aa7c131b
vfrezende <vitorfranco12@gmail.com>
2020-10-20 10:08:05 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -24,7 +24,8 @@ public class Customer {
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
         Rental each = (Rental) rentals.nextElement();
-        double thisAmount = amountFor(each);
+        //determine amounts for each line
+        double thisAmount = each.getCharge();
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -44,25 +45,4 @@ public class Customer {
              " frequent renter points";
      return result;
    }
-
-  //determine amounts for each line
-  private double amountFor(Rental aRental) {
-    double thisAmount = 0;
-    switch (aRental.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-        thisAmount += 2;
-        if (aRental.getDaysRented() > 2)
-            thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-        break;
-      case Movie.NEW_RELEASE:
-        thisAmount += aRental.getDaysRented() * 3;
-        break;
-      case Movie.CHILDRENS:
-        thisAmount += 1.5;
-        if (aRental.getDaysRented() > 3)
-            thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-          break;
-    }
-    return thisAmount;
-  }
 } 
