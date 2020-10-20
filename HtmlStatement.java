bc988ff0eaf80d2abfa4159dd9af493ac53d4995
vfrezende <vitorfranco12@gmail.com>
2020-10-20 10:28:32 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,22 +1,41 @@
 import java.util.Enumeration;
 
 public class HtmlStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-        "</EM></H1><P>\n";
-        while (rentals.hasMoreElements()) {
-           Rental each = (Rental) rentals.nextElement();
-           //show figures for each rental
-           result += each.getMovie().getTitle()+ ": " +
-           String.valueOf(each.getCharge()) + "<BR>\n";
-        }
-        //add footer lines
-        result += "<P>You owe <EM>" +
-        String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" + 
-        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-        "</EM> frequent renter points<P>";
-        return result;
-     }
+   public String value(Customer aCustomer) {
+      Enumeration rentals = aCustomer.getRentals();
+      String result = makeHeaderMessage(aCustomer);
+
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         //show figures for each rental
+         result += makeRentalMessage(each);
+      }
+
+      //add footer lines
+      result += makeOweMessage(aCustomer);
+      result += makePointsMessage(aCustomer);
+      return result;
+   }
+
+   public String makeHeaderMessage(Customer aCustomer) {
+      return "<H1>Rentals for <EM>" + aCustomer.getName() +
+      "</EM></H1><P>\n";
+   }
+
+   public String makeRentalMessage(Rental rental) {
+      return rental.getMovie().getTitle()+ ": " +
+             String.valueOf(rental.getCharge()) + "<BR>\n";
+   }
+
+   public String makeOweMessage(Customer aCustomer) {
+      return "<P>You owe <EM>" +
+             String.valueOf(aCustomer.getTotalCharge()) + 
+             "</EM><P>\n";
+   }
+
+   public String makePointsMessage(Customer aCustomer) {
+      return "On this rental you earned <EM>" + 
+             String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+             "</EM> frequent renter points<P>";
+   }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,22 +1,40 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = "Rental Record for " + aCustomer.getName() +
-        "\n";
-        while (rentals.hasMoreElements()) {
-           Rental each = (Rental) rentals.nextElement();
-           //show figures for this rental
-           result += "\t" + each.getMovie().getTitle()+ "\t" +
-           String.valueOf(each.getCharge()) + "\n";
-        }
-        //add footer lines
-        result += "Amount owed is " +
-        String.valueOf(aCustomer.getTotalCharge()) + "\n";
-        result += "You earned " +
-        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-        " frequent renter points";
-        return result;
-     }
+   public String value(Customer aCustomer) {
+      Enumeration rentals = aCustomer.getRentals();
+      String result = makeHeaderMessage(aCustomer);
+
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         //show figures for each rental
+         result += makeRentalMessage(each);
+      }
+
+      //add footer lines
+      result += makeOweMessage(aCustomer);
+      result += makePointsMessage(aCustomer);
+      return result;
+   }
+
+   public String makeHeaderMessage(Customer aCustomer) {
+      return "Rental Record for " + aCustomer.getName() +
+      "\n";
+   }
+
+   public String makeRentalMessage(Rental rental) {
+      return "\t" + rental.getMovie().getTitle()+ "\t" +
+                String.valueOf(rental.getCharge()) + "\n";
+   }
+
+   public String makeOweMessage(Customer aCustomer) {
+      return "Amount owed is " +
+             String.valueOf(aCustomer.getTotalCharge()) + "\n";
+   }
+
+   public String makePointsMessage(Customer aCustomer) {
+      return "You earned " +
+             String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+             " frequent renter points";
+   }
 }
